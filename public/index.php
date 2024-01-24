1a04d14aedac868c8e3f99b06c0a1e78f3960de5
dumasg <geremydumas+github@gmail.com>
2024-01-24 12:04:02 +0100

    add front controller

--- public/index.php
+++ public/index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo('test');
+include '../config/database.php';
+
+$routerArray = [
+    "accueil"
+];
+
+$router = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_URL);
+$router = $router ?? "accueil";
+
+
+if(array_search($router, $routerArray) !== false){
+    $i = array_search($router, $routerArray);
+    require "./" . $routerArray[$i] . ".php";
+}else{
+    require "./" . "notfound.php";
+}
