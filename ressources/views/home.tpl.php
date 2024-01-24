c636c828602b6cda5495a4d75106cc58202c7328
dumasg <geremydumas+github@gmail.com>
2024-01-24 16:17:08 +0100

    ajout du css et modification de l'affichage avec ajout d'un lien dans home.tpl

--- ressources/views/home.tpl.php
+++ ressources/views/home.tpl.php
@@ -3,12 +3,16 @@
 //var_dump($result);
 ?>
 
-<?php foreach ($result as $key => $value){ ?>
-    <div style="border: 1px solid black; margin-top: 20px">
-        <h1>Title : <?= $value['title'] ?></h1>
-        <p><?= $value['content'] ?></p>
-        <h4>Auteur : <?= $value['name'] ?></h4>
-    </div>
-<?php } ?>
+<div class="container-articles">
+    <?php foreach ($result as $key => $value){ ?>
+        <a class="card-article" style="text-decoration: none" href="/?action=blogPost&id=<?= $value['id'] ?>">
+            <div style="border: 1px solid black; margin-top: 20px">
+                <h1>Title : <?= $value['title'] ?></h1>
+                <p><?= $value['content'] ?></p>
+                <h4>Auteur : <?= $value['name'] ?></h4>
+            </div>
+        </a>
+    <?php } ?>
+</div>
 
 <?php require "../ressources/views/layouts/footer.tpl.php"; ?>
